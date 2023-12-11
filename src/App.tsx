812d3eeead515cfcc8ec498f75d856845d24a143
Wout Junius <wout.junius@gmail.com>
2023-12-11 09:34:02 +0100

    feat: Adding a ID input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import ApiCall from "./components/api-call";
 import { apiCalls } from "./data/api-calls/nestjs";
 
 function App() {
+  const [meetupId, setMeetupId] = useState("");
   return (
     <>
       <h1 className="text-6xl font-extrabold px-10 pt-10">NestJS demo calls</h1>
+
       <div className="p-10">
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Meetup ID?</span>
+          </div>
+          <input
+            type="text"
+            value={meetupId}
+            onChange={(e) => setMeetupId(e.target.value)}
+            placeholder="Meetup ID"
+            className="input input-bordered w-full max-w-xs"
+          />
+        </label>
+
         {apiCalls.map((call) => (
           <ApiCall
             key={call.title}
@@ -14,7 +30,7 @@ function App() {
             endpoint={call.endpoint}
             content={call.content}
             call={() => {
-              return fetch(`api${call.endpoint}`, {
+              return fetch(`api${call.endpoint}`.replace(":id", meetupId), {
                 method: call.method,
                 headers: {
                   "Content-Type": "application/json",
--- src/components/api-call.tsx
+++ src/components/api-call.tsx
@@ -28,7 +28,7 @@ export default function ApiCall({
   };
 
   return (
-    <div className="collapse collapse-plus bg-base-200 m-4">
+    <div className="collapse collapse-plus bg-base-200 my-4">
       <input type="radio" name="my-accordion-3" />
       <div className="collapse-title text-xl font-medium">
         <span
